f40a3918e0b79c7cddd77340d77045f3c1bb53a7
TgT60 <dima.icigo@yandex.ru>
2023-01-25 22:53:23 +0300

    Start project

--- HelperAPI/HelperAPI/Startup.cs
+++ HelperAPI/HelperAPI/Startup.cs
@@ -3,19 +3,26 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using HelperAPI.Service;
 
 namespace HelperAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
         public void ConfigureServices(IServiceCollection services)
         {
-            
-
+            services.AddSwaggerDocument();
+            Configuration.Bind("Project",new Config());
+            services.AddControllersWithViews()   // подключаем контролеры для паттерна MVC
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider(); // установка совместимости версий
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -23,16 +30,19 @@ namespace HelperAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUi3();
+
             }
 
             app.UseRouting();
 
+            app.UseStaticFiles();       // добовляем поддержку файлов типа (js,css,img и т.д) 
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default","{controller=Home}/{action=Index}/{id?}");
+               
             });
         }
     }
