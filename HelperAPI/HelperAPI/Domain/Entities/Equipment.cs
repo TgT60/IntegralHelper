627cd0265f15bd42616e88e93d324dd655776852
TgT60 <dima.icigo@yandex.ru>
2023-01-27 03:45:20 +0300

    Create class Equipment for entity

---
+++ HelperAPI/HelperAPI/Domain/Entities/Equipment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelperAPI.Domain.Entities
+{
+    public class Equipment
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Img { get; set; }
+
+        public string Discription { get; set; }
+    }
+}
