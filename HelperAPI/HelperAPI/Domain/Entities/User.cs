12694bccfd0d150298c87723bd97f6ed29560d26
TgT60 <dima.icigo@yandex.ru>
2023-01-27 03:33:15 +0300

    Create User class for entity

---
+++ HelperAPI/HelperAPI/Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelperAPI.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public string SureName { get; set; }
+        
+        public string PersonImg { get; set; }
+
+    }
+}
